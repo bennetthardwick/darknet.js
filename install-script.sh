1fa3ba3e8c97d786694bc819f6947ff0d1d8c846
Bennett Hardwick <bennetthardwick@icloud.com>
2018-06-25 21:12:11 +1000

    Update: install-script to allow easy reinstall
    Currently the install-script checks to see if the libdarknet file
    already exists, and if it does, immediately exits the installation
    rather than continuing. Since we're now suggesting users can
    toggle/enable CUDA, it would be good if users could issue `npm rebuild
    darknet` after exporting the correct flags.

--- install-script.sh
+++ install-script.sh
@@ -1,53 +1,46 @@
 #!/usr/bin/env bash
+if [ ! -d darknet ]; then
+		git clone https://github.com/pjreddie/darknet;
 
-if [ ! -f libdarknet.so ]; then
+		if [ $? -ne 0 ]; then
+				echo "Could not clone darknet repo";
+				exit 1;
+		fi
+fi
 
-    if [ ! -d darknet ]; then
-        git clone https://github.com/pjreddie/darknet;
+# dive in the darknet folder and make
+cd darknet
 
-        if [ $? -ne 0 ]; then
-            echo "Could not clone darknet repo";
-            exit 1;
-        fi
-    fi
+# look for exported variables for GPU and CUDNN
+GPU="${DARKNET_BUILD_WITH_GPU:-0}";
+CUDNN="${DARKNET_BUILD_WITH_CUDNN:-0}";
 
-    # dive in the darknet folder and make
-    cd darknet
+case "$GPU" in
+		1|0);;
+		*) echo "Interpreting DARKNET_BUILD_WITH_GPU=$GPU as 0"; GPU=0;;
+esac
+case "$CUDNN" in
+		1|0);;
+		*) echo "Interpreting DARKNET_BUILD_WITH_CUDNN=$CUDNN as 0"; CUDNN=0;;
+esac
 
-    # look for exported variables for GPU and CUDNN
-    GPU="${DARKNET_BUILD_WITH_GPU:-0}";
-    CUDNN="${DARKNET_BUILD_WITH_CUDNN:-0}";
+sed -i -e "s/GPU=[01]/GPU=${GPU}/g" ./Makefile
+sed -i -e "s/CUDNN=[01]/CUDNN=${CUDNN}/g" ./Makefile
 
-    case "$GPU" in
-        1|0);;
-        *) echo "Interpreting DARKNET_BUILD_WITH_GPU=$GPU as 0"; GPU=0;;
-    esac
-    case "$CUDNN" in
-        1|0);;
-        *) echo "Interpreting DARKNET_BUILD_WITH_CUDNN=$CUDNN as 0"; CUDNN=0;;
-    esac
+make
 
-    sed -i -e "s/GPU=[01]/GPU=${GPU}/g" ./Makefile
-    sed -i -e "s/CUDNN=[01]/CUDNN=${CUDNN}/g" ./Makefile
+if [ $? -ne 0 ]; then
+		echo "Could not compile darknet";
+		exit 2;
+fi
 
-    make
+# copy lib
+cp libdarknet* ..
 
-    if [ $? -ne 0 ]; then
-        echo "Could not compile darknet";
-        exit 2;
-    fi
+# dive out
+cd ..
 
-    # copy lib
-    cp libdarknet* ..
-
-    # dive out
-    cd ..
-
-    # if macos make .dylib symlink
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        ln -s libdarknet.so libdarknet.dylib
-    fi
-
-else
-    echo "Already built darknet"
+# if macos make .dylib symlink
+if [[ "$OSTYPE" == "darwin"* ]]; then
+		ln -s libdarknet.so libdarknet.dylib
 fi
