392a8e8e162ca0348e5f30b971c4b6ac2cf35800
Bennett Hardwick <bennetthardwick@icloud.com>
2018-08-18 10:52:08 +1000

    Fixed: builds not working for OPENCV/MP

--- install-script.sh
+++ install-script.sh
@@ -14,18 +14,33 @@ cd darknet
 # look for exported variables for GPU and CUDNN
 GPU="${DARKNET_BUILD_WITH_GPU:-0}";
 CUDNN="${DARKNET_BUILD_WITH_CUDNN:-0}";
+OPENCV="${DARKNET_BUILD_WITH_OPENCV:-0}";
+OPENMP="${DARKNET_BUILD_WITH_OPENMP:-0}";
 
 case "$GPU" in
 		1|0);;
 		*) echo "Interpreting DARKNET_BUILD_WITH_GPU=$GPU as 0"; GPU=0;;
 esac
+
 case "$CUDNN" in
 		1|0);;
 		*) echo "Interpreting DARKNET_BUILD_WITH_CUDNN=$CUDNN as 0"; CUDNN=0;;
 esac
 
+case "$OPENCV" in
+		1|0);;
+		*) echo "Interpreting DARKNET_BUILD_WITH_OPENCV=$OPENCV as 0"; OPENCV=0;;
+esac
+
+case "$OPENMP" in
+		1|0);;
+		*) echo "Interpreting DARKNET_BUILD_WITH_OPENMP=$OPENMP as 0"; OPENMP=0;;
+esac
+
 sed -i -e "s/GPU=[01]/GPU=${GPU}/g" ./Makefile
 sed -i -e "s/CUDNN=[01]/CUDNN=${CUDNN}/g" ./Makefile
+sed -i -e "s/OPENCV=[01]/OPENCV=${GPU}/g" ./Makefile
+sed -i -e "s/OPENMP=[01]/OPENMP=${GPU}/g" ./Makefile
 
 make
 
